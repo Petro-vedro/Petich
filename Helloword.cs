fd8f24f107f98ca26fa49f178606082497aab07b
Petro-vedro <119161032+Petro-vedro@users.noreply.github.com>
2022-11-27 15:15:29 +0500

    Add files via upload

---
+++ Helloword.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Привет мир
+        print("Hello, Word!");
+         
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
